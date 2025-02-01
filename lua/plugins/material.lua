a7f382e786e2c2a5f2ec07f570fcfc8ed481c933
minhd-vu <minhd_vu@yahoo.com>
2025-02-01 01:16:18 -0500

    fix(material): use opts instead of config

--- lua/plugins/material.lua
+++ lua/plugins/material.lua
@@ -2,37 +2,35 @@
 return {
   {
     "marko-cerovac/material.nvim",
-    config = function()
-      require("material").setup {
-        contrast = {
-          terminal = true, -- Enable contrast for the built-in terminal
-          sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-          floating_windows = false, -- Enable contrast for floating windows
-          cursor_line = false, -- Enable darker background for the cursor line
-          non_current_windows = true, -- Enable darker background for non-current windows
-          filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
-        },
-        plugins = { -- Uncomment the plugins that you use to highlight them
-          -- Available plugins:
-          "dap",
-          "dashboard",
-          "gitsigns",
-          -- "hop",
-          "indent-blankline",
-          -- "lspsaga",
-          -- "mini",
-          "neogit",
-          "neorg",
-          "nvim-cmp",
-          -- "nvim-navic",
-          -- "nvim-tree",
-          "nvim-web-devicons",
-          -- "sneak",
-          "telescope",
-          -- "trouble",
-          "which-key",
-        },
-      }
-    end,
-  }
+    opts = {
+      contrast = {
+        terminal = true, -- Enable contrast for the built-in terminal
+        sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        floating_windows = false, -- Enable contrast for floating windows
+        cursor_line = false, -- Enable darker background for the cursor line
+        non_current_windows = true, -- Enable darker background for non-current windows
+        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+      },
+      plugins = { -- Uncomment the plugins that you use to highlight them
+        -- Available plugins:
+        "dap",
+        "dashboard",
+        "gitsigns",
+        -- "hop",
+        "indent-blankline",
+        -- "lspsaga",
+        -- "mini",
+        "neogit",
+        "neorg",
+        "nvim-cmp",
+        -- "nvim-navic",
+        -- "nvim-tree",
+        "nvim-web-devicons",
+        -- "sneak",
+        "telescope",
+        -- "trouble",
+        "which-key",
+      },
+    },
+  },
 }
