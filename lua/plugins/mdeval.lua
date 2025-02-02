8640f20545087f13d6446148d938bda9afeea4af
minhd-vu <minhd_vu@yahoo.com>
2025-02-02 02:24:20 -0500

    feat: mdeval

---
+++ lua/plugins/mdeval.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "https://github.com/jubnzv/mdeval.nvim",
+    config = function()
+      require("mdeval").setup {
+        require_confirmation = false,
+        eval_options = {},
+      }
+    end,
+    cmd = "MdEval",
+    keys = {
+      {
+        "<Leader>me",
+        "<cmd>lua require 'mdeval'.eval_code_block()<cr>",
+        mode = { "n" },
+        desc = "Evaluate code block",
+      },
+    },
+  },
+}
