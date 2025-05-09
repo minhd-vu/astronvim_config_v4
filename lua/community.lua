60e6fb57cff124a1185ab97eabd34d78d6ae8546
minhd-vu <minhd_vu@yahoo.com>
2025-05-09 16:28:34 -0400

    fix: remove neogen

--- lua/community.lua
+++ lua/community.lua
@@ -12,7 +12,6 @@ return {
   { import = "astrocommunity.docker.lazydocker" },
   { import = "astrocommunity.editing-support.dial-nvim" },
   { import = "astrocommunity.editing-support.hypersonic-nvim" },
-  { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.register.nvim-neoclip-lua" },
   { import = "astrocommunity.search.grug-far-nvim" },
