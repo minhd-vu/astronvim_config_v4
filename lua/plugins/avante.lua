8d7bb8c222f059c6630f795fab74568971cd9018
minhd-vu <minhd_vu@yahoo.com>
2025-05-09 16:43:33 -0400

    fix: use lowercase a for avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,9 +1,36 @@
+local prefix = "<Leader>a"
+
+---@type LazySpec
 return {
   "yetone/avante.nvim",
   opts = {
     provider = "openai",
     openai = {
       model = "gpt-4o",
+    },
+    mappings = {
+      ask = prefix .. "<CR>",
+      edit = prefix .. "e",
+      refresh = prefix .. "r",
+      focus = prefix .. "f",
+      select_model = prefix .. "?",
+      stop = prefix .. "S",
+      select_history = prefix .. "h",
+      toggle = {
+        default = prefix .. "t",
+        debug = prefix .. "d",
+        hint = prefix .. "H",
+        suggestion = prefix .. "s",
+        repomap = prefix .. "R",
+      },
+      diff = {
+        next = "]c",
+        prev = "[c",
+      },
+      files = {
+        add_current = prefix .. ".",
+        add_all_buffers = prefix .. "B",
+      },
     },
   },
 }
